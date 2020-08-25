e1c524c357e521132d23979c384cd76c43ebf3f3
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-08-26 02:58:03 +0300

    complete first task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String args[]){
-
+    public static void main(String[] args){
         //calculate factorial (n!)
         int i, factorialNumber = 1;
         int number = 4;
