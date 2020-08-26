2484fb6f58bbb2f5c9e2a7fb0aec01c02db0b640
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-08-26 23:06:35 +0300

    2.1 not-working 2.2 complete

---
+++ src/main/java/arraysCollections/primeNumbersList.java
@@ -0,0 +1,18 @@
+package arraysCollections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class primeNumbersList {
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[25];
+
+
+
+
+
+    }
+
+}
