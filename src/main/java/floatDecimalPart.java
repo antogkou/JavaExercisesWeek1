da66742b92ce79041958f52b119e0e1ff147c0aa
Antonis Gkoutzamanis <mythoc@gmail.com>
2020-08-26 13:21:32 +0300

    complete exercise 6 complete simple algorithms slide

---
+++ src/main/java/floatDecimalPart.java
@@ -0,0 +1,17 @@
+//6. Given a positive float number, print its decimal part. For example, given the number 2.31, the output should be 0.31.
+
+public class floatDecimalPart {
+
+    public static void main(String[] args) {
+
+        float number = (float) 2.34454;
+        String floatAsString = String.valueOf(number);
+        int indexOfFloat = floatAsString.indexOf(".");
+        if (number >= 0){
+            System.out.println("Float Number: " + number);
+            System.out.println("Integer Part: " + floatAsString.substring(0, indexOfFloat));
+            System.out.println("Decimal Part: 0" + floatAsString.substring(indexOfFloat));
+
+        }
+    }
+}
